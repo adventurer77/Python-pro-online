f3f354373f2c1c66c9c2b546917eb6956524c5dc
Volodymyr Tsypliak <vtsypliak.w@gmail.com>
2025-01-11 12:24:13 +0200

    add lines of documentation

--- lesson_6/tests/test_student.py
+++ lesson_6/tests/test_student.py
@@ -1,22 +1,11 @@
 import unittest
-from task1_group.human import Human 
 from task1_group.student import Student
 
 
 class TestStudent(unittest.TestCase):
 
-    # def setUp(self):
-    #     human = Human("Den", "Don", 22)
-
-    # def test_create_student_valid(self):
-    #     # We check the correct creation of the object
-
-    #     student = Student(human, "1b", "Java")
-    #     self.assertEqual(student.group, "1d")
-    #     self.assertEqual(student.course, "Java")
-
     def test_create_student_valid(self):
-        # Коректне створення об'єкта
+        # Correct object creation
         student = Student("John", "Doe", 20, "ABC-123", "Math")
         self.assertEqual(student.first_name, "John")
         self.assertEqual(student.second_name, "Doe")
@@ -25,46 +14,42 @@ class TestStudent(unittest.TestCase):
         self.assertEqual(student.course, "Math")
 
     def test_create_student_invalid_group(self):
-        #
+        # Checking for an incorrect group
         with self.assertRaises(ValueError) as context:
             Student("Den", "Don", 22, "", "Java")
         self.assertEqual(str(context.exception), "Group must be a non-empty string.")
 
     def test_create_student_invalid_course(self):
-        #
+        # Check for wrong course
         with self.assertRaises(ValueError) as context:
             Student("Den", "Don", 22, "1d", "")
         self.assertEqual(str(context.exception), "Course must be a non-empty string.")
 
     def test_set_valid_group(self):
-        #
+        # Checking for the correct group
         student = Student("Den", "Don", 22, "1b", "Java")
         student.group = "2f"
         self.assertEqual(student.group, "2f")
 
     def test_set_invalid_group(self):
-        #
+        # Checking the wrong group during the shift
         student = Student("Den", "Don", 22, "1b", "Java")
         with self.assertRaises(ValueError) as context:
             student.group = ""
-        self.assertEqual(
-            str(context.exception), "Group must be a non-empty string."
-        )
+        self.assertEqual(str(context.exception), "Group must be a non-empty string.")
 
     def test_set_valid_course(self):
-        #
+        # Checking for the correct course
         student = Student("Den", "Don", 22, "1b", "Java")
         student.course = "Python"
         self.assertEqual(student.course, "Python")
 
     def test_set_invalid_course(self):
-        #
+        # Checking the wrong course during the shift
         student = Student("Den", "Don", 22, "1b", "Java")
         with self.assertRaises(ValueError) as context:
             student.course = ""
-        self.assertEqual(
-            str(context.exception), "Course must be a non-empty string."
-        )
+        self.assertEqual(str(context.exception), "Course must be a non-empty string.")
 
     def test_str_representation(self):
         # We check __str__ method
